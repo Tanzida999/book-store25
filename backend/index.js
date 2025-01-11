5fca90fe9dacfb2b7d2a7e271132e726b4c4cbc7
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-01-11 19:35:09 +0600

    Update and Delete

--- backend/index.js
+++ backend/index.js
@@ -43,9 +43,63 @@ app.post("/books", async (request, response) => {
 //Route for get all books from Database
 app.get("/books", async (request, response) => {
   try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.log(error.messaase);
-    response.status(501);
+    response.status(500).send({ message: error.message });
+  }
+});
+//Route for get one book from database by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.messaase);
+    response.status(500).send({ message: error.message });
+  }
+});
+//Route for Update a Book
+app.put("/book/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required feilds: title, author, publishYear",
+      });
+    }
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "Book Not Found" });
+    }
+    return response.status(200).send({ message: "Book Updated Successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+//Route for Delete a Book
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await Book.findByIdAndDelete(id);
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    }
+    return response.status(200).send({ message: "Book Deleted Successfully" });
+  } catch (error) {
+    console.log(error.messaage);
+    response.statusMessage(500).send({ messsage: error.message });
   }
 });
 mongoose
