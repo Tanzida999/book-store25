e70f6966f8650a85fa99c532f7680261b00716b9
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-01-12 19:08:22 +0600

    Updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBooks from "./pages/CreateBooks";
+import DeleteBooks from "./pages/DeleteBooks";
+import ShowBook from "./pages/showBook";
+import EditBooks from "./pages/EditBooks";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/books/create" element={<CreateBooks></CreateBooks>}></Route>
+      <Route path="/books/details/:id" element={<ShowBook></ShowBook>}></Route>
+      <Route path="/books/edit/:id" element={<EditBooks></EditBooks>}></Route>
+      <Route
+        path="/books/delete/:id"
+        element={<DeleteBooks></DeleteBooks>}
+      ></Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowerRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <BrowerRouter>
+    <App />
+  </BrowerRouter>
+);
